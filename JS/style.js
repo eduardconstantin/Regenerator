786b79c91cbd6be0e71631c3b0b5d4a3e46a90e1
eddietheviper <ibinceanu.eduard@yahoo.com>
2016-07-30 11:52:18 +0300

    Initial Commit

---
+++ JS/style.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+    Height();
+    $( ".beggin" ).click(function() {
+        
+        setTimeout(function() {
+            demo.init();
+            $( "canvas" ).fadeIn( 5000, function() {});
+        }, 19500);
+       
+       $( ".beggin" ).fadeOut( "slow", function() {});
+       setTimeout(function() {
+            $( ".bg" ).fadeOut( 3000, function() {});
+            setTimeout(function() {
+               $(".audio").trigger('play');
+            }, 2000);
+       }, 18000);
+
+       setTimeout(function() {
+            $( ".text h1" ).fadeIn( 1000, function() {});
+       }, 500);
+       setTimeout(function() {
+            $( ".text h1" ).fadeOut( 1000, function() {});
+       }, 5500);
+
+       setTimeout(function() {
+            $( ".text p.first" ).fadeIn( 1500, function() {});
+       }, 4500);
+       setTimeout(function() {
+            $( ".text p.first" ).fadeOut( 2000, function() {});
+       }, 9000);
+
+       setTimeout(function() {
+            $( ".hands" ).fadeIn( 5000, function() {});
+       }, 5000);
+       setTimeout(function() {
+            $( ".hands" ).fadeOut( 2000, function() {});
+       }, 16000);
+
+       setTimeout(function() {
+            $( ".text p.second" ).fadeIn( 3000, function() {});
+       }, 9800);
+       setTimeout(function() {
+            $( ".text p.second" ).fadeOut( 2000, function() {});
+       }, 16000);
+
+       
+    });
+
+});
+
+$(window).on('load resize', function() {
+
+});    
+
+/* ==================================================
+HEIGHT
+================================================== */
+function Height() {
+    var windowHeight = ($(window).height());
+    var windowWidth = ($(window).width());
+
+    if (windowHeight >= 600) {
+        if (windowWidth < 974) {
+            $('header').css('height', 500);
+        } else {
+            $('header').css('height', windowHeight);
+        }
+    } else {
+        if (windowWidth < 974) {
+            $('header').css('height', 500);
+        } else {
+            $('header').css('height', windowHeight);
+        }
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+}).listen(process.env.PORT);
