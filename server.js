460d94d5fd6742c15511e4e7b0fff8d2017ec52b
eddietheviper <ibinceanu.eduard@yahoo.com>
2016-07-30 12:37:42 +0300

    test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+    res.end('<h1>Test Bitch!</h1>');
 }).listen(process.env.PORT);
