f6941851915239a23ec98f3b483a9f8a97fb9591
eddietheviper <ibinceanu.eduard@yahoo.com>
2016-07-30 12:44:34 +0300

    test2

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Test Bitch!</h1>');
-}).listen(process.env.PORT);
