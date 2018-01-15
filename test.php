6425decbd7a64dc8c726ad0efcf1c648363c2b10
Edy TheViper <ibinceanu.eduard@yahoo.com>
2018-01-15 19:00:02 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+    $serverName = "serverwebtest.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "webtest",
+        "Uid" => "testadmin",
+        "PWD" => "Admintest18"
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+            FROM [SalesLT].[ProductCategory] pc
+            JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
