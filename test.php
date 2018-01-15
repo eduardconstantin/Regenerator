5264c8e50ec1b0ac4915e2e15562003244724516
Edy TheViper <ibinceanu.eduard@yahoo.com>
2018-01-15 20:23:05 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ $password = 'Admintest18';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+mysqli_connect($conn, $host, $username, $password);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
