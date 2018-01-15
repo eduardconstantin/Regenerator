ed64b03815bab8634ecb4e9cbe3204c8fa3102ab
Edy TheViper <ibinceanu.eduard@yahoo.com>
2018-01-15 20:12:41 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,18 +1,17 @@
 <?php
-$servername = "tcp:serverwebtest.database.windows.net,1433";
-$username = "testadmin";
-$password = "Admintest18";
-
-// Create connection
-$conn = new PDO($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-else{ 
-    echo "Connected successfully";
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:serverwebtest.database.windows.net,1433; Database = webtest", "testadmin", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "testadmin@serverwebtest", "pwd" => "{your_password_here}", "Database" => "webtest", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:serverwebtest.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 ?>
